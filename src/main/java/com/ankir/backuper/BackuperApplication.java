bbbd86772273bddb4120585ad5b5caca165e1120
SHCHERBA92 <vony4ka666@rambler.ru>
2025-01-27 22:42:19 +0300

    first commit

---
+++ src/main/java/com/ankir/backuper/BackuperApplication.java
@@ -0,0 +1,13 @@
+package com.ankir.backuper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackuperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackuperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ankir/backuper/BackuperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankir.backuper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackuperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
